2806ebe1a05630012912f076cdcb36ad664606df
Shaishav Paudel <morganhouse15@gmail.com>
2023-04-10 16:08:30 +0545

    RecipeApp AngularUniversalCompleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ import { RecipeEffects } from './recipes/store/recipe.effects';
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     CoreModule,
     HttpClientModule,
